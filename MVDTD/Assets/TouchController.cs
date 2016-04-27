bcb996fcdc9dbe918fd767f6f28fd82a8d9c284a
Sergio Malonda <sergio.del93@gmail.com>
2016-04-27 20:32:20 +0200

    Drag and Drop Draggables

--- MVDTD/Assets/TouchController.cs
+++ MVDTD/Assets/TouchController.cs
@@ -4,8 +4,13 @@ using UnityEngine.EventSystems;
 
 public class TouchController : MonoBehaviour  {
 
+	//Declares the camera
 	private Camera _camera;
-	private Transform gameObjectToDrag;
+
+
+	//Declares the game object to be dragged
+	[SerializeField]
+	private GameObject gameObjectToDrag;
 
 	[SerializeField]
 	private float speedMovement;
@@ -37,17 +42,25 @@ public class TouchController : MonoBehaviour  {
 					Ray ray = Camera.main.ScreenPointToRay(pos);
 					RaycastHit hitInfo;
 
-
 					if(Physics.Raycast(ray, out hitInfo))
 					{
+						
+						//If we hit something, the camera will be blocked
 						_camera.GetComponent<CameraController>().BlockCamera = true;
 
+						if(hitInfo.collider.GetComponent<Draggable>() != null)
+						{
+							gameObjectToDrag = hitInfo.transform.gameObject;
+							Debug.Log(gameObjectToDrag);
+							dist = hitInfo.transform.position.z - _camera.transform.position.z;
+							v3 = new Vector3(pos.x, pos.y, pos.z);
+							v3 = Camera.main.ScreenToWorldPoint(v3);
+							offset = gameObjectToDrag.transform.position - v3;
 
-						gameObjectToDrag = hitInfo.transform;
-						dist = hitInfo.transform.position.z - _camera.transform.position.z;
-						v3 = new Vector3(pos.x, pos.y, pos.z);
-						v3 = Camera.main.ScreenToWorldPoint(v3);
-						offset = gameObjectToDrag.transform.position - v3;
+						}
+
+
+
 
 						
 					}
@@ -55,16 +68,20 @@ public class TouchController : MonoBehaviour  {
 					break;
 				case TouchPhase.Moved:
 
-					v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-					v3 = Camera.main.ScreenToWorldPoint(v3);
-					gameObjectToDrag.transform.position = v3 + offset;
+					if(gameObjectToDrag != null)
+					{
+						v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+						v3 = Camera.main.ScreenToWorldPoint(v3);
+						gameObjectToDrag.transform.position = v3 + offset;
 
-					_camera.GetComponent<CameraController>().BlockCamera = true;
+						_camera.GetComponent<CameraController>().BlockCamera = true;
+					}
+
 	
 					break;
 				case TouchPhase.Ended:
 
-					Debug.Log("Enter");
+					gameObjectToDrag = null;
 					_camera.GetComponent<CameraController>().BlockCamera = false;
 
 					break;
