43652723f47b05896bd7d077b9bc748c7816a2c2
Sergio Malonda <sergio.del93@gmail.com>
2016-05-03 00:01:15 +0200

    Layeeeeeeeeeeeeeeeeeeeers

--- MVDTD/Assets/TurretBehavior.cs
+++ MVDTD/Assets/TurretBehavior.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class TurretBehavior : MonoBehaviour {
 
+	public LayerMask myLayerMask;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,18 +12,35 @@ public class TurretBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
-		int myLayerMask = ~(1 << 10);
-		//layerMask = ~layerMask
-
+			
+		/*
 		RaycastHit[] hits;
 
-		hits = Physics.RaycastAll(transform.position, transform.right);
+		hits = Physics.RaycastAll(transform.position, transform.right, myLayerMask);
+		Debug.Log(hits.Length);
+		Debug.DrawRay(transform.position, transform.right);
+		Debug.Log(hits[0].collider.gameObject.name);
+
+		for(int i = 0; i < hits.Length; i++)
+		{
+			//RaycastHit hit = hits[i];
+			//Debug.Log(hit.collider.gameObject.name);
+		}
+		*/
+		//RaycastHit hit;
+
+		/*
+		if(Physics.Raycast(transform.position, transform.right, out hit, 100, ~myLayerMask))
+		{
+			Debug.Log(hit.transform.name);
+		}
+		*/
+		RaycastHit [] hits = Physics.RaycastAll(transform.localPosition, transform.right, 100, ~myLayerMask);
 
 		for(int i = 0; i < hits.Length; i++)
 		{
 			RaycastHit hit = hits[i];
-			Debug.Log(hit.collider.gameObject.name);
+			Debug.Log(hit.transform.name);
 		}
 	
 	}
