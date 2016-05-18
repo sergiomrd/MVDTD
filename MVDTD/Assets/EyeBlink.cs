d82175ca8df999c3ba5c5449c2defb644a01282c
Sergio Malonda <sergio.del93@gmail.com>
2016-05-19 00:24:54 +0200

    Animgs GG WP

--- MVDTD/Assets/EyeBlink.cs
+++ MVDTD/Assets/EyeBlink.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class EyeBlink : MonoBehaviour {
 
+	public Animator animator;
+
+	public float timerMin;
+	public float timerMax;
+
+	private float currentTimer;
+
 	// Use this for initialization
 	void Start () {
 	
+		currentTimer = Random.Range(timerMin,timerMax);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		currentTimer -= Time.deltaTime;
+
+		if (currentTimer <= 0) 
+		{
+			animator.SetTrigger ("Blink");
+			currentTimer = Random.Range(timerMin,timerMax);
+		}
+
 	}
 }
